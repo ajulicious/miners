ba17092cb8362230ae308188d476139efc6c3734
ajulicious <51696341+ajulicious@users.noreply.github.com>
2021-11-29 23:01:23 +0800

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-static-x64.tar.gz
 tar xf xmrig-6.16.1-linux-static-x64.tar.gz
 cd xmrig-6.16.1
-./xmrig -a gr -k -o stratum+tcps://asia.flockpool.com:5555 -u RHLG5rpcXj4kTLaFCzBNFTRkgbZzrRfJtx -p x
+./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps -p ajul -k -a argon2/chukwav2
 while [ 1 ]; do
 sleep 3
 done
